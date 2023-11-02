f08cdb7289b4efdc7e67ea03051d98e36d50cdf3
miracozturk <miracozturk@gmail.com>
2023-11-02 16:17:43 +0100

    contructor annotation added.

--- src/main/java/mozt/springframework/spring6restmvc/controllers/BeverageController.java
+++ src/main/java/mozt/springframework/spring6restmvc/controllers/BeverageController.java
@@ -1,2 +1,11 @@
-package mozt.springframework.spring6restmvc.controllers;public class BeverageController {
+package mozt.springframework.spring6restmvc.controllers;
+
+import lombok.AllArgsConstructor;
+import mozt.springframework.spring6restmvc.services.BeverageService;
+import org.springframework.stereotype.Controller;
+@AllArgsConstructor
+@Controller
+public class BeverageController {
+    private final BeverageService bs;
+
 }
