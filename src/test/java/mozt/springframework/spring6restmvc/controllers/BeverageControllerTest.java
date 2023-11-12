dfe62af43c224d8ed011fcaf8be77830de607dcf
miracozturk <miracozturk@gmail.com>
2023-11-13 00:52:03 +0100

    test methods for BeverageController class

--- src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
+++ src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
@@ -1,18 +1,38 @@
 package mozt.springframework.spring6restmvc.controllers;
 
+
+import mozt.springframework.spring6restmvc.services.BeverageService;
 import org.junit.jupiter.api.Test;
+
+import org.junit.platform.engine.TestExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import java.util.UUID;
 
-@SpringBootTest
+
+
+@WebMvcTest(BeverageController.class)
 class BeverageControllerTest {
 
     @Autowired
-    BeverageController bc;
+    MockMvc mockmvc;
+
+    @MockBean
+    BeverageService bs;
+
     @Test
-    void getBeverageById() {
-        System.out.println(this.bc.getBeverageById(UUID.randomUUID()));
+    public void getBeverageById() throws Exception {
+        this.mockmvc.perform(get("/api/v1/beverage/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
+
 }
