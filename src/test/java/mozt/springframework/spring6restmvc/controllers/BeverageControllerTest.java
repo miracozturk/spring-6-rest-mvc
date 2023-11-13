3186695df7cc0b1193978c29f27d7c6a05b06bdd
miracozturk <miracozturk@gmail.com>
2023-11-13 01:10:09 +0100

    mockito given added.

--- src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
+++ src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
@@ -2,6 +2,8 @@ package mozt.springframework.spring6restmvc.controllers;
 
 
 import mozt.springframework.spring6restmvc.services.BeverageService;
+import mozt.springframework.spring6restmvc.services.BeverageServiceImpl;
+import mozt.springframework.spring6restmvc.model.Beverage;
 import org.junit.jupiter.api.Test;
 
 import org.junit.platform.engine.TestExecutionResult;
@@ -11,8 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 import java.util.UUID;
@@ -28,11 +34,17 @@ class BeverageControllerTest {
     @MockBean
     BeverageService bs;
 
+    BeverageServiceImpl bsImp = new BeverageServiceImpl();
+
     @Test
     public void getBeverageById() throws Exception {
+        Beverage b = this.bsImp.listBeverages().get(0);
+
+        given(bs.getBeverageById(any(UUID.class))).willReturn(b);
         this.mockmvc.perform(get("/api/v1/beverage/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
