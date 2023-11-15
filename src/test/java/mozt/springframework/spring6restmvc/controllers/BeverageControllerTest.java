dc01817c1708c13aec748dd15f88aa05d32f2104
miracozturk <miracozturk@gmail.com>
2023-11-15 23:52:01 +0100

    http save Beverage method was added.

--- src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
+++ src/test/java/mozt/springframework/spring6restmvc/controllers/BeverageControllerTest.java
@@ -3,16 +3,14 @@ package mozt.springframework.spring6restmvc.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 import mozt.springframework.spring6restmvc.services.BeverageService;
 import mozt.springframework.spring6restmvc.services.BeverageServiceImpl;
 import mozt.springframework.spring6restmvc.model.Beverage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.platform.engine.TestExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,20 +18,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.core.Is.is;
-
-
-import java.util.ArrayList;
-import java.util.UUID;
-
 
 
 @WebMvcTest(BeverageController.class)
 class BeverageControllerTest {
 
     @Autowired
-    MockMvc mockmvc;
+    MockMvc mockMvc;
 
     @MockBean
     BeverageService bs;
@@ -41,11 +35,27 @@ class BeverageControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    BeverageServiceImpl bsImp = new BeverageServiceImpl();
+    BeverageServiceImpl bsImp;
+
+    @BeforeEach
+    void setup(){
+        this.bsImp = new BeverageServiceImpl();
+    }
 
     @Test
-    void testCreateNewBeer() throws JsonProcessingException {
+    void testCreateNewBeer() throws Exception {
         Beverage b = this.bsImp.listBeverages().get(0);
+        b.setId(null);
+        b.setVersion(null);
+        given(this.bs.saveNewBeverage(any(Beverage.class))).willReturn(this.bsImp.listBeverages().get(1));
+        mockMvc.perform(post("/api/v1/beverage")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(b))
+                                )
+                                .andExpect(status().isCreated())
+                                .andExpect(header().exists("Location"));
+
         System.out.println(objectMapper.writeValueAsString(b));
 
     }
@@ -53,7 +63,7 @@ class BeverageControllerTest {
     @Test
     void testListBeverages() throws Exception {
         given(bs.listBeverages()).willReturn(bsImp.listBeverages());
-        mockmvc.perform(get("/api/v1/beverage").accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beverage").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON) )
                 .andExpect(jsonPath("$.length()", is(3)));
@@ -64,7 +74,7 @@ class BeverageControllerTest {
         Beverage testB = this.bsImp.listBeverages().get(0);
 
         given(bs.getBeverageById(testB.getId())).willReturn(testB);
-        this.mockmvc.perform(get("/api/v1/beverage/" + testB.getId())
+        this.mockMvc.perform(get("/api/v1/beverage/" + testB.getId())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
