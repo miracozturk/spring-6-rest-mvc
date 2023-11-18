9b834949f1ee0a4d2ed38bc571b7661d51ae74ee
miracozturk <miracozturk@gmail.com>
2023-11-18 22:27:37 +0100

    moch-mvc delete customer test was added.

--- src/test/java/mozt/springframework/spring6restmvc/controllers/CustomerControllerTest.java
+++ src/test/java/mozt/springframework/spring6restmvc/controllers/CustomerControllerTest.java
@@ -1,21 +1,26 @@
 package mozt.springframework.spring6restmvc.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mozt.springframework.spring6restmvc.model.Beverage;
 import mozt.springframework.spring6restmvc.model.Customer;
 import mozt.springframework.spring6restmvc.services.CustomerService;
 import mozt.springframework.spring6restmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -31,6 +36,22 @@ class CustomerControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Test
+    void testDeleteBeverage() throws Exception {
+
+        Customer c = this.custServImp.listCustomers().get(0);
+
+        mockMvc.perform(delete("/api/v1/customer/" + c.getId())
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+
+        ArgumentCaptor<UUID> uuidCaptor = ArgumentCaptor.forClass(UUID.class);
+        verify(this.cs).deleteCustomerById(uuidCaptor.capture());
+
+        assertThat(c.getId()).isEqualTo(uuidCaptor.getValue());
+
+    }
 
 
     @Test
